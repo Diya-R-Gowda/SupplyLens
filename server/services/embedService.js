82d643b2ade80c5d040bc341d72eb3095a8c562f
Diya <diyagowda1427@gmail.com>
2026-07-06 12:09:29 +0530

    feat(services): add generateAnswer function using gemini-1.5-flash

--- server/services/embedService.js
+++ server/services/embedService.js
@@ -7,3 +7,11 @@ exports.getEmbeddings = async (text) => {
   const result = await model.embedContent(text);
   return result.embedding.values; // Returns the float array
 };
+
+//For RAG
+exports.generateAnswer = async (prompt) => {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  return response.text();
+};
